82bc9fc8e9614c0dffed553f4c2daa5b0ad5ac05
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-09-14 22:39:14 +0300

    Update Repository.java
    implemented CRUD

--- e-shop/src/main/java/student/example/orm/repositories/Repository.java
+++ e-shop/src/main/java/student/example/orm/repositories/Repository.java
@@ -6,11 +6,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import student.example.orm.entities.DummyEntity;
 import student.example.orm.entities.Entity;
 
 public abstract class Repository {
     
-    private final String url = "jdbc:postgresql://localhost:postgres/e_shop?user=postgres&password=Laser112&ssl=false";
+    private final String url = "jdbc:postgresql://localhost:postgres:5432/e_shop?user=postgres&password=Laser112&ssl=false";
     protected Connection conn;
 
     public Repository(){
@@ -38,11 +39,13 @@ public abstract class Repository {
         Entity entity = null;
         try {
             st = conn.createStatement();
-            ResultSet rs= st.executeQuery("SELECT *FROM products where id="+ id);
+            ResultSet rs= st.executeQuery("SELECT *FROM Entity where id="+ id);
             rs.next();
-            entity = new Entity(
+            entity = (Entity) new DummyEntity(
                 rs.getInt(1),
-                rs.getString(2));
+                rs.getString(2),
+                rs.getString(3));
+            
             } catch (SQLException e) {
                
             e.printStackTrace();
@@ -51,12 +54,30 @@ public abstract class Repository {
     }
     
     public void update (Entity entity){
+       
+          
+        Statement st;
+        try {
+            st = conn.createStatement();
+            st.executeUpdate("UPDATE Entity SET createdat ='"+ entity.getCreatedAt()+  "' WHERE id = " +entity.getId());
+        } catch (SQLException e) {
+            
+            e.printStackTrace();
+        }
         
     }
     
     public void delete (Entity entity){
-        
+        Statement st;
+        try {
+            st = conn.createStatement();
+            st.executeUpdate("DELETE FROM Entity WHERE id ="+ entity.getId());
+        } catch (SQLException e) {
+            
+            e.printStackTrace();
+        }
     }
     
 
 }
+
