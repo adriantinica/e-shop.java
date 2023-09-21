a95ad63fe4a9fb5bdfb8c15728fb748acf0ea4cf
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-09-21 19:28:00 +0300

    Update Repository.java

--- e-shop/src/main/java/student/example/orm/repositories/Repository.java
+++ e-shop/src/main/java/student/example/orm/repositories/Repository.java
@@ -1,17 +1,22 @@
 package student.example.orm.repositories;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import student.example.orm.entities.DummyEntity;
 import student.example.orm.entities.Entity;
+import student.example.utils.NamingConventionTransformer;
 
 public abstract class Repository {
     
-    private final String url = "jdbc:postgresql://localhost:5432/e_shop?user=postgres&password=Laser112&ssl=false";
+    //this is an object of type Field
+    private final NamingConventionTransformer nct = new NamingConventionTransformer();
+    private final String url = "jdbc:postgresql://localhost:5433/e_shop?user=postgres&password=laser112&ssl=false";
     protected Connection conn;
 
     public Repository(){
@@ -21,23 +26,53 @@ public abstract class Repository {
             e.printStackTrace();
         }
     }
-
+     // this is an object of type Method
+     //                      |
+     //                      v
     public void create (Entity entity){
+
         Statement st;
+
+        //using REFLECTION API
+        Class<?> entityClass =  entity.getClass();
+        String entityClassName = entityClass.getName();
+        String [] parts = entityClassName.split("\\.");
+        entityClassName = parts[parts.length-1];
         try {
 
             //using REFLECTION API
-            // 1. get the entity class name :
-            String entityClassName = entity.getClass().getName();
-            String [] parts = entityClassName.split("\\.");
-            entityClassName = parts[parts.length-1];
+            // 1. get the entity class name 
             System.out.println("class name: "+entityClassName);
 
-
-
             st = conn.createStatement();
-            st.executeUpdate("INSERT into Entity VALUES("+entity.getId()+",'"+ entityClassName +"','"+ entity.getCreatedAt()+"')");
+            st.executeUpdate("INSERT into "+nct.snakeToPascal("Entity")+" VALUES("+entity.getId()+",'"+ entityClassName +"','"+ entity.getCreatedAt()+"')");
         } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        
+        
+
+        
+        try {
+            st = conn.createStatement();
+            String sql = ("INSERT into "+ nct.snakeToPascal(entityClassName)+" VALUES("+ entity.getId());
+
+            Method[] methods = entityClass.getDeclaredMethods();
+            for (Method method : methods) {
+             if (method.getName().startsWith("get")) {
+               if (method.getReturnType().getName().equals("java.lang.String")) {
+                 sql += ",'"+ method.invoke(entity)+ "'";
+                } else{
+                 sql += ","+ method.invoke(entity)+ "";
+                }
+              }
+            }  
+            
+            sql += ")";
+            st.executeUpdate(sql);
+         
+        } catch (SQLException | IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
         }
         
@@ -45,22 +80,35 @@ public abstract class Repository {
     
     // to be continued:
     public Entity read (int id){
-        Statement st;
         Entity entity = null;
+        
         try {
-            st = conn.createStatement();
-            ResultSet rs= st.executeQuery("SELECT *FROM Entity where id="+ id);
-            rs.next();
-            entity = (Entity) new DummyEntity(
-                rs.getInt(1),
-                rs.getString(2),
-                rs.getString(3));
             
-            } catch (SQLException e) {
-               
-            e.printStackTrace();
-        }
-        return entity;
+            Statement st;
+            st= conn.createStatement();
+            String entityClassName = "Dummyentity";
+            String sql = "SELECT * FROM " + nct.snakeToPascal(entityClassName)+ " JOIN" + 
+            " " + nct.snakeToPascal("Entity")+ " ON " 
+                + nct.snakeToPascal(entityClassName) + ".entity_id =" 
+                + nct.snakeToPascal("Entity")+ ".id WHERE " 
+                + nct.snakeToPascal("Entity")+".id = " + id;
+            ResultSet rs = st.executeQuery(sql);
+            rs.next();
+            
+            //loop through columns use setters:
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int size = rsmd.getColumnCount();
+            for(int i=1; i<= size; i++){
+                String columnName = nct.pascalToSnake(rsmd.getColumnName(i));
+                System.out.println(columnName+": "+  rs.getObject(i));
+            }
+
+       } catch (SQLException e) {
+           e.printStackTrace();
+       }
+       return entity;
+
+        
     }
     
     public void update (Entity entity){
