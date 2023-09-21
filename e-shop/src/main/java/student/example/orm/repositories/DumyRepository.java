c352880e30708077c954f836339d006fb0827b1e
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-09-21 22:59:29 +0300

    Update DumyRepository.java

--- e-shop/src/main/java/student/example/orm/repositories/DumyRepository.java
+++ e-shop/src/main/java/student/example/orm/repositories/DumyRepository.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 import student.example.orm.entities.DummyEntity;
 import student.example.orm.entities.Entity;
 
-public class DumyRepository extends Repository {
+public class DummyRepository extends Repository {
 
 
 
@@ -16,37 +16,7 @@ public class DumyRepository extends Repository {
 
    
 
-    @Override
-    public Entity read(int id) {
-        
-        Statement st;
-        
-        Entity entity = null;
-        try {
-             st = conn.createStatement();
-            ResultSet rs= st.executeQuery("SELECT *FROM dummy_entity WHERE entity_id="+ id);
-            rs.next();
-            entity =  new DummyEntity(
-                rs.getInt(1),
-                rs.getString(2),
-                rs.getString(3),
-                rs.getString(4)
-                );
-            
-            } catch (SQLException e) {
-               
-            e.printStackTrace();
-        
-
-            if (entity == null) {
-                 entity = super.read(id);
-            
-            }
-           
-        }
-        return entity;
-        
-    }
+    
 
     @Override
     public void update(Entity entity) {
@@ -91,6 +61,7 @@ public class DumyRepository extends Repository {
     
     
 }
+
 
 
 
